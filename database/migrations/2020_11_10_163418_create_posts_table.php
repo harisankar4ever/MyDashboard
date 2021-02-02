98d48abc8e6bb8ddd743b2d576c2dca3362f6f35
Harisankar.H <harisankar4ever@gmail.com>
2021-02-02 14:42:05 +0300

    commit on main

--- database/migrations/2020_11_10_163418_create_posts_table.php
+++ database/migrations/2020_11_10_163418_create_posts_table.php
@@ -19,6 +19,7 @@ class CreatePostsTable extends Migration
             $table->string('description');
             $table->string('sdesc');
             $table->text('body');
+            $table->string('category_id');
 
             $table->timestamps();
         });
